1425bcca1fce0aeb1ab20ae30cf0184e92908fc2
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-10-06 11:06:09 +0530

    implement logout functionality

--- logout.php
+++ logout.php
@@ -1,13 +1,5 @@
-<?php  ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Logout | Cars CRUD</title>
-    </head>
-    <body>
-        <?php include "navigation.php"; ?>
-        <h1>Logout</h1>
-
-    </body>
-</html>
+<?php
+    session_start();
+    session_destroy();
+    header('Location: index.php');
+?>
