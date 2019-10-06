b467fda3379b2bc4e18c32359f5f1f84af25ee6c
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-10-06 10:25:44 +0530

    create blank login and logout pages with navigation links in them

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Login | Cars CRUD</title>
+    </head>
+    <body>
+        <?php include "navigation.php"; ?>
+        <h1>Login</h1>
+
+    </body>
+</html>
---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Logout | Cars CRUD</title>
+    </head>
+    <body>
+        <?php include "navigation.php"; ?>
+        <h1>Logout</h1>
+
+    </body>
+</html>
