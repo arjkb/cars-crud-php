9ca13aa61e81386bc946b46599ed8a5a2cf55f74
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-10-06 10:48:37 +0530

    submit username and password through the form

--- login.php
+++ login.php
@@ -1,4 +1,21 @@
-<?php  ?>
+<?php
+    session_start();
+    require_once("pdo.php");
+
+    // print_r($_POST['useremail']); echo "<br>";
+    // var_dump(isset($_POST['useremail'])); echo "<br>";
+    // print_r($_POST['password']);  echo "<br>";
+    // var_dump(isset($_post['password']));  echo "<br>";
+    if( isset($_POST['useremail']) && isset($_POST['password']) )  {
+
+        // echo "valid!";
+        $useremail = $_POST['useremail'];
+        $password = $_POST['password'];
+        echo "User email = $useremail <br>";
+        echo "User password = $password <br>";
+    }
+    // echo "invalid!";
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -8,6 +25,12 @@
     <body>
         <?php include "navigation.php"; ?>
         <h1>Login</h1>
+
+        <form action="login.php" method="post">
+            <input type="text" name="useremail" placeholder="E-Mail" ><br><br>
+            <input type="text" name="password" placeholder="Password" ><br><br>
+            <button type="submit">Log In</button>
+        </form>
 
     </body>
 </html>
